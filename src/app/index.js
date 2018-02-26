b1c477ba05f9c879775dfab2469933bc4107285a
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-26 16:30:38 +0100

    components

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-alert('It works!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var TodoComponent = React.createClass({
+    render() {
+        return(
+            <h1>Hello!</h1>
+        )
+    }
+})
+
+ReactDOM.render(<TodoComponent />, document.getElementById('app'))
