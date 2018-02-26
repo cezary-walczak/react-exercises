2dabc7cffcb62263888fdfddc009e1938e53925c
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-02-26 21:10:01 +0100

    changing state

--- src/app/index.js
+++ src/app/index.js
@@ -4,18 +4,25 @@ import ReactDOM from 'react-dom'
 var TodoComponent = React.createClass({
     getInitialState() {
         return {
-            todos: ['wash up', 'eat something', 'take a nap']
+            todos: ['wash up', 'eat something', 'take a nap'],
+            age: 30
         }
     },
     render() {
+        var ager = setTimeout(() => {
+            this.setState({
+                age: 35
+            })
+        }, 3000)
         return(
             <div>
+                <p>{this.state.age}</p>
                 <ul>
                     <li>{this.state.todos[0]}</li>
                     <li>{this.state.todos[1]}</li>
                     <li>{this.state.todos[2]}</li>
                 </ul>
-                <ListComponent todos={this.state.todos}/>
+                {/* <ListComponent todos={this.state.todos}/> */}
             </div>
         )
     }
